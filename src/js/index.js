a23e7ee0b12a94232b25e145ad464724a6931ffd
vsparrow <khan@vsparrow.com>
2018-04-16 21:44:32 -0400

    Item class added

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,7 @@
    const urlbase = "http://127.0.0.1:3000/api/v1/"
    let url = urlbase + "categories"
    // fetch(url).then(res=>res.json()).then(json=>console.log(json))
-   fetch(url).then(res=>res.json()).then(json=>parseJSON(json))
+   fetch(url).then(res=>res.json()).then(json=>parseJSONIntoClasses(json))
    /////////////////////////////////////////////////////////////////////////////class category, move to category.js
    const CategoryAll = [];
    class Category{
@@ -16,15 +16,41 @@
      }//constructor
      static all(){ return CategoryAll}
    }//Category
-
+   /////////////////////////////////////////////////////////////////////////////class Item, move to Item.js
+   const ItemAll = []
+   class Item{
+     constructor(category,id,title,brand,image,size,price,description){
+       this.category = category
+       this.id = id;
+       this.title = title;
+       this.brand = brand;
+       this.image = image;
+       this.size = size;
+       this.price = price;
+       this.description = description
+       ItemAll.push(this)
+     }//constructor
+     static all(){return ItemAll}
+   }//Item
    /////////////////////////////////////////////////////////////////////////////move to category.js ?????
-   function parseJSON(json){
-     json.forEach(function(obj){
-       let category = new Category(obj.id,obj.name)
-     })
+   function parseJSONIntoClasses(json){
+     console.log("THIS IS ALL JSON:");
+     console.log(json);
+     json.forEach(function(category){
+       let newCategory = new Category(category.id,category.name)
+       category.items.forEach(function(item){
+         //chose category.id over an instance below because question:
+         // is it a pointer to the instance? or a new instance? It is the pointer
+         //because CategoryAll[0] === ItemAll[0].category is true
+         let newItem = new Item(newCategory,item.id,item.title,item.brand,item.image,item.size,item.price) //////////////ADD DESCRIPTION BY SENDING IT VIA JSON  THEN ADDD TO HERE
+       })//forEach item
+     })//forEach category aka category
      console.log("CategoryAll");
      console.log(CategoryAll.length)
      console.log(CategoryAll)
+     console.log("ItemAll");
+     console.log(ItemAll);
+     // debugger
    }
 
  })//document.addEventListener
