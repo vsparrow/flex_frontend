2a44595a445e9c58191277dfba77a1aad75a50f8
vsparrow <khan@vsparrow.com>
2018-04-19 10:57:32 -0400

    move sortItemsByPrice() into item.js

--- src/js/index.js
+++ src/js/index.js
@@ -234,30 +234,30 @@
 //   // debugger
 //   displayItems(category)
 // }
-/////////////////////////////////////////////////////////////////////////////   display sort high
-function sortItemsByPrice(howtosort){
-  //get all col from page //add to array?
-  // howtosort = "high"
-  let allrows = document.querySelectorAll("#main .container .row .col-md-3")
-  // console.log(allrows);
-  let allrowsarray = Array.from(allrows)
-  console.log(allrowsarray);
-  console.log(allrowsarray[0].lastChild.innerText); //$1278
-  // allrowsarray.forEach(el => console.log(Number(el.lastChild.innerText.split("$").join(" ")) ))
-  //sort by price
-  let sortedArray = allrowsarray.sort(function(a,b){
-    // return Number(a.lastChild.innerText.split("$").join(" ")).localeCompare(Number(b.lastChild.innerText.split("$").join(" ")))
-    if(howtosort=="high"){
-      return  (Number(b.lastChild.innerText.split("$").join(" "))) -Number(a.lastChild.innerText.split("$").join(" "))
-    }
-    else {
-      return Number(a.lastChild.innerText.split("$").join(" ")) - (Number(b.lastChild.innerText.split("$").join(" ")))
-    }
-  })
-  console.log(sortedArray);
-  //redisplay
-  redisplay(sortedArray)
-}//end sortItemsByPrice
+// /////////////////////////////////////////////////////////////////////////////   display sort high
+// function sortItemsByPrice(howtosort){
+//   //get all col from page //add to array?
+//   // howtosort = "high"
+//   let allrows = document.querySelectorAll("#main .container .row .col-md-3")
+//   // console.log(allrows);
+//   let allrowsarray = Array.from(allrows)
+//   console.log(allrowsarray);
+//   console.log(allrowsarray[0].lastChild.innerText); //$1278
+//   // allrowsarray.forEach(el => console.log(Number(el.lastChild.innerText.split("$").join(" ")) ))
+//   //sort by price
+//   let sortedArray = allrowsarray.sort(function(a,b){
+//     // return Number(a.lastChild.innerText.split("$").join(" ")).localeCompare(Number(b.lastChild.innerText.split("$").join(" ")))
+//     if(howtosort=="high"){
+//       return  (Number(b.lastChild.innerText.split("$").join(" "))) -Number(a.lastChild.innerText.split("$").join(" "))
+//     }
+//     else {
+//       return Number(a.lastChild.innerText.split("$").join(" ")) - (Number(b.lastChild.innerText.split("$").join(" ")))
+//     }
+//   })
+//   console.log(sortedArray);
+//   //redisplay
+//   redisplay(sortedArray)
+// }//end sortItemsByPrice
 
 // ////////////////////////////////////////////////////////////////////////////////redisplay()
 // //called by sortItemsByPrice()
