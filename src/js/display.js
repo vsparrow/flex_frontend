67d00dacabd53034177da8bef82eb0f9072b84ea
vsparrow <khan@vsparrow.com>
2018-04-19 11:18:03 -0400

    cleanup display.js

--- src/js/display.js
+++ src/js/display.js
@@ -1,8 +1,5 @@
 function displayItem(id){
-  //get item with id of id
   let item = ItemAll.find(item => item.id == id)
-  // console.log(item);
-  //clear main
   let main = document.querySelector("#main")
   let itemhtml = ""
   main.innerHTML = ""
@@ -19,56 +16,45 @@ function displayItem(id){
   itemhtml += `<br><br><br><br><br>`
   itemhtml += `<p>DESCRIPTION</p>`
   itemhtml += `<p style="font-size:.9em">${item.description}</p>`
-  // itemhtml += `<span></span>`
   itemhtml += `</div>` //end col-md-4
-  // itemhtml += `<div class="col-md-12"><img src="${image}" class=""></div>`
   itemhtml += `</div></div>` //close container and row
-  //display on main
   main.innerHTML = itemhtml
 }
 
 
-/////////////////////////////////////////////////////////////////////////////displayItems
+////////////////////////////////////////////////////////////////////////////////displayItems
 function displayItems(category="all"){
-  // navShowDefault() //reset navbar buttons if changed
-  navShowCategories() //decided that best to display categories, as the logo would display home and items, and we dont have profile
+  navShowCategories() //if item with new category added, navbar would update
   let displayhtml = ""
-  // displayhtml += `<div class="container"><div class="row fp-row-items">`
-  displayhtml += `<div class="container">` //
-  // if(category = all){}
+  displayhtml += `<div class="container">`
   let count = 0
-  ItemAll.forEach(function(item){                                           //add itemID and link to item
-
+  ItemAll.forEach(function(item){
    if(category == "all" || category == item.category.name.toLowerCase()){
      if(count==0){displayhtml += `<div class="row fp-row-items">`}
       let image = "./src/image/defaultflex.jpg"
       if (item.image != ""){image = item.image}
-      let itemhtml = `<div class="col-md-3" align="center" style="padding-top: 15px; padding-bottom: 15px" data-fp-grid-item="${item.id}">`  //move style to css
+      let itemhtml = `<div class="col-md-3" align="center" style="padding-top: 15px; padding-bottom: 15px" data-fp-grid-item="${item.id}">`
       itemhtml += `<img src="${image}" class="img-responsive" data-fp-image-item="${item.id}" data-fp-grid-item="${item.id}">`
       itemhtml += `<br><strong data-fp-grid-item="${item.id}">${item.brand}</strong>`
-      //////
       let shortTitle= ""
       if(item.title){
         if (item.title.length > 40){ shortTitle= item.title.substring(0,32)+"..."}  //move to own function?
         else {shortTitle= item.title}
         itemhtml += `<br><span data-fp-grid-item="${item.id}" style="font-size: .9em">${shortTitle}</span>`
       }
-      //////
       itemhtml += `<br><span data-fp-grid-item="${item.id}">$${item.price}</span>`
       itemhtml += `</div><!-- END COL-->`
       displayhtml += itemhtml
       if (count == 3){displayhtml += `<br></div><!-- END ROW-->`}
-      // console.log("count is :" +count);
       ++count;
       if (count == 4){ count = 0}
     }//end if category == all
-
-    })//forEach
-    displayhtml += `</div><!-- CLOSE CONTAINER -->` //close container and row
+  })//forEach
+  displayhtml += `</div><!-- CLOSE CONTAINER -->` //close container and row
   document.querySelector('#main').innerHTML = ""
   document.querySelector('#main').innerHTML += displayhtml
   addEventListenerFrontPageItems()
-}//display
+}//displayItems
 ////////////////////////////////////////////////////////////////////////////////redisplay()
 //called by sortItemsByPrice()
 function redisplay(arrayOfElements){
