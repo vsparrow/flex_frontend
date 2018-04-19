b47380ab2af2b61d4fdaa008f4d786edbd24c4a2
vsparrow <khan@vsparrow.com>
2018-04-19 10:12:06 -0400

    move displayItemss() to display.js

--- src/js/index.js
+++ src/js/index.js
@@ -99,47 +99,47 @@
  //   //display on main
  //   main.innerHTML = itemhtml
  // }
- /////////////////////////////////////////////////////////////////////////////displayItems
- function displayItems(category="all"){
-   // navShowDefault() //reset navbar buttons if changed
-   navShowCategories() //decided that best to display categories, as the logo would display home and items, and we dont have profile
-   let displayhtml = ""
-   // displayhtml += `<div class="container"><div class="row fp-row-items">`
-   displayhtml += `<div class="container">` //
-   // if(category = all){}
-   let count = 0
-   ItemAll.forEach(function(item){                                           //add itemID and link to item
-
-    if(category == "all" || category == item.category.name.toLowerCase()){
-      if(count==0){displayhtml += `<div class="row fp-row-items">`}
-       let image = "./src/image/defaultflex.jpg"
-       if (item.image != ""){image = item.image}
-       let itemhtml = `<div class="col-md-3" align="center" style="padding-top: 15px; padding-bottom: 15px" data-fp-grid-item="${item.id}">`  //move style to css
-       itemhtml += `<img src="${image}" class="img-responsive" data-fp-image-item="${item.id}" data-fp-grid-item="${item.id}">`
-       itemhtml += `<br><strong data-fp-grid-item="${item.id}">${item.brand}</strong>`
-       //////
-       let shortTitle= ""
-       if(item.title){
-         if (item.title.length > 40){ shortTitle= item.title.substring(0,32)+"..."}  //move to own function?
-         else {shortTitle= item.title}
-         itemhtml += `<br><span data-fp-grid-item="${item.id}" style="font-size: .9em">${shortTitle}</span>`
-       }
-       //////
-       itemhtml += `<br><span data-fp-grid-item="${item.id}">$${item.price}</span>`
-       itemhtml += `</div><!-- END COL-->`
-       displayhtml += itemhtml
-       if (count == 3){displayhtml += `<br></div><!-- END ROW-->`}
-       // console.log("count is :" +count);
-       ++count;
-       if (count == 4){ count = 0}
-     }//end if category == all
-
-     })//forEach
-     displayhtml += `</div><!-- CLOSE CONTAINER -->` //close container and row
-   document.querySelector('#main').innerHTML = ""
-   document.querySelector('#main').innerHTML += displayhtml
-   addEventListenerFrontPageItems()
- }//display
+ // /////////////////////////////////////////////////////////////////////////////displayItems
+ // function displayItems(category="all"){
+ //   // navShowDefault() //reset navbar buttons if changed
+ //   navShowCategories() //decided that best to display categories, as the logo would display home and items, and we dont have profile
+ //   let displayhtml = ""
+ //   // displayhtml += `<div class="container"><div class="row fp-row-items">`
+ //   displayhtml += `<div class="container">` //
+ //   // if(category = all){}
+ //   let count = 0
+ //   ItemAll.forEach(function(item){                                           //add itemID and link to item
+ //
+ //    if(category == "all" || category == item.category.name.toLowerCase()){
+ //      if(count==0){displayhtml += `<div class="row fp-row-items">`}
+ //       let image = "./src/image/defaultflex.jpg"
+ //       if (item.image != ""){image = item.image}
+ //       let itemhtml = `<div class="col-md-3" align="center" style="padding-top: 15px; padding-bottom: 15px" data-fp-grid-item="${item.id}">`  //move style to css
+ //       itemhtml += `<img src="${image}" class="img-responsive" data-fp-image-item="${item.id}" data-fp-grid-item="${item.id}">`
+ //       itemhtml += `<br><strong data-fp-grid-item="${item.id}">${item.brand}</strong>`
+ //       //////
+ //       let shortTitle= ""
+ //       if(item.title){
+ //         if (item.title.length > 40){ shortTitle= item.title.substring(0,32)+"..."}  //move to own function?
+ //         else {shortTitle= item.title}
+ //         itemhtml += `<br><span data-fp-grid-item="${item.id}" style="font-size: .9em">${shortTitle}</span>`
+ //       }
+ //       //////
+ //       itemhtml += `<br><span data-fp-grid-item="${item.id}">$${item.price}</span>`
+ //       itemhtml += `</div><!-- END COL-->`
+ //       displayhtml += itemhtml
+ //       if (count == 3){displayhtml += `<br></div><!-- END ROW-->`}
+ //       // console.log("count is :" +count);
+ //       ++count;
+ //       if (count == 4){ count = 0}
+ //     }//end if category == all
+ //
+ //     })//forEach
+ //     displayhtml += `</div><!-- CLOSE CONTAINER -->` //close container and row
+ //   document.querySelector('#main').innerHTML = ""
+ //   document.querySelector('#main').innerHTML += displayhtml
+ //   addEventListenerFrontPageItems()
+ // }//display
   //////////////////////////////////////////////////////////////////////////////addEventListenerSiteTitle
  function addEventListenerSiteTitle(){
    document.querySelector("#site-title").addEventListener("click",function(){
